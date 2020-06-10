ecf3721c3ddf0c8f684c05ec461357a7228193d5
unknown <laravel5.6vue2.0@gmail.com>
2020-06-10 15:53:04 +0600

    user table update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->tinyInteger('role_id')->default(1)->index();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
